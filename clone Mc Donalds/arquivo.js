a8f9fe4472c430b262bd50ae3157ca0ac1a2356b
LeoLaranja <129693232+LeoLaranja@users.noreply.github.com>
2023-11-07 20:40:03 -0300

    add new file

---
+++ clone Mc Donalds/arquivo.js
@@ -0,0 +1,44 @@
+let nome= "Leonardo"
+let sobrenome = "Hansen"
+let nomeCompleto = (nome + ' ' + sobrenome)
+console.log(nomeCompleto)
+//string simples acima
+
+let cachorro="vira-lata"
+let cor= "branco"
+let vacinado = true
+let cão = (cachorro + ' ' + cor)
+console.log(cão)
+console.log(vacinado)
+
+let id = "Leonardo"
+let sobname= "Laranja"
+let nameCompl = (id + ' ' + sobname)
+console.log(nameCompl)
+
+let a= "Leonardo"
+let b = "Hansen"
+let ab = `${a} ${b}`
+console.log(ab)
+
+let ida= 35
+let qtd = 12
+let soma = (ida + qtd + 10)
+console.log(soma)
+
+let idade = 16
+if(idade >= 18) {
+    console.log("Esse cara é MAIOR de idade")
+} else {
+    console.log("Esse cara é MENOR de idade")
+}
+
+let animal = "leão"
+let color = "rosa"
+if(animal == "leão" , color == "rosa"){
+    console.log("esse animal é selvagem")
+} else {
+    console.log ("esse animal é domesticado")
+}
+
+
